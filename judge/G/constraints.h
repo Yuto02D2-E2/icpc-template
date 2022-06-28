a072f098ca2c45dc59a0b071824edfa5ffd06fde
Yuto02D2-E2 <d.yuto.k.b@gmail.com>
2022-06-29 00:25:37 +0900

    [delete] judge/G/* judge/H/*

--- judge/G/constraints.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const long long int MIN_N = 2;
-const long long int MAX_N = 3000;
-const long long int MIN_C = -1000000000;
-const long long int MAX_C = 1000000000;
-const long long int MAX_CASES = 50;
--- judge/H/testlib.h
+++
@@ -1,1 +0,0 @@
-// Download from https://raw.githubusercontent.com/MikeMirzayanov/testlib/6daa79fdc9c2b113be8d40f828e3dc81e67ce33d/testlib.h
